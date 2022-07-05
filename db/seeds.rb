5c174b53408c3c12a348ecd2a0520b2d3fbaf8d1
Sheykly Oxlaj <oxlajsheylt@gmail.com>
2022-07-05 09:46:49 -0700

    adds data to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+song = Song.new(title: "Pure Souls", album: "Donda", artist: "Ye", year:2021 , album_cover: "https://i.scdn.co/image/ab67616d0000b273cad190f1a73c024e5a40dddd")
+song.save
+
+song = Song.new(title: "Oui", album: "Late Nights", artist: "Jeremih", year: 2015, album_cover: "https://media.pitchfork.com/photos/5929b28213d197565213a7a8/1:1/w_600/23346138.jpg")
+song.save
