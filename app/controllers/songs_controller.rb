2814713205c8558b5305ac74712b82957273f67c
Sheykly Oxlaj <oxlajsheylt@gmail.com>
2022-07-05 10:29:40 -0700

    creates song_controller actions, index, show, create, update, destroy

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,4 +4,41 @@ class SongsController < ApplicationController
     songs = Song.all
     render json: (songs.as_json)
   end 
+
+  def show 
+    song = Song.find_by(id: params["id"])
+    render json: (song.as_json)
+  end 
+
+  def create
+    song = Song.new(
+      title: params[:title]
+      album: params[:album]
+      artist: params[:artist]
+      year: params[:year]
+      album_cover: params[:album_cover]
+    )
+    song.save
+    render json: (song.as_json)
+  end 
+
+  def update
+    song = Song.find_by(:id params["id"])
+    song.title = params[:title] || song.title
+    song.album = params[:album] || song.album
+    song.artist = params[:artist] || song.artist
+    song.year = params[:year] || song.year
+    song.album_cover = params[:album_cover] || song.album_cover
+
+    song.save
+    render json: (song.as_json)
+  end 
+
+  def destroy
+    song = Song.find_by(:id params["id"])
+    song.destroy
+    
+    render json: {message: "song destroyed succesfully" }
+  end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
 
  get "/songs" => "songs#index"
 
+ get "/songs/:id" => "songs#show"
+
+ 
+
+
 end
