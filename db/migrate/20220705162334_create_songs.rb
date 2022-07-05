6cece072ff7f97f6b74de54b76ebdf7a84e5580d
Sheykly Oxlaj <oxlajsheylt@gmail.com>
2022-07-05 09:30:51 -0700

    Adds file; generates model

---
+++ db/migrate/20220705162334_create_songs.rb
@@ -0,0 +1,14 @@
+class CreateSongs < ActiveRecord::Migration[7.0]
+  def change
+    create_table :songs do |t|
+      t.string :title
+      t.string :album
+      t.string :artist
+      t.integer :year
+      t.string :album_cover
+
+      t.timestamps
+    end
+  end
+end
+g
